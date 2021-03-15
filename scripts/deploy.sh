be575164d6c663fabfecc1cdb4b0e72f7fae0d10
Noah-Vincenz Noeh <noah-vincenz.noeh@kcl.ac.uk>
2021-03-15 10:39:16 +0000

    Added -a flag to heroku release and removed app line from deploy block in travis.

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,5 +1,4 @@
 echo $HEROKU_API_KEY | docker login --username $USERNAME --password-stdin registry.heroku.com
 docker tag $USERNAME/todo-app:latest registry.heroku.com/devops-project-exercise-8/web
 docker push registry.heroku.com/devops-project-exercise-8/web
-heroku git:remote -a devops-project-exercise-8
-heroku container:release web
+heroku container:release web -a devops-project-exercise-8
