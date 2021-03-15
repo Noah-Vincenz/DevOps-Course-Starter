c19ccc140a3be21ccdbf107df7084c0bb124e8a9
Noah-Vincenz Noeh <noah-vincenz.noeh@kcl.ac.uk>
2021-03-15 14:36:32 +0000

    Added app name var to bash script.

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,4 +1,5 @@
 echo $HEROKU_API_KEY | docker login --username $USERNAME --password-stdin registry.heroku.com
-docker tag $USERNAME/todo-app:latest registry.heroku.com/devops-project-exercise-8/web
-docker push registry.heroku.com/devops-project-exercise-8/web
-heroku container:release web -a devops-project-exercise-8
+APP_NAME="devops-project-exercise-8"
+docker tag $USERNAME/todo-app:latest registry.heroku.com/$APP_NAME/web
+docker push registry.heroku.com/$APP_NAME/web
+heroku container:release web -a $APP_NAME
